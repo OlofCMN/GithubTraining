531e07d80a260863a64fbc182d324c278e08609b
olof <olof.cm.nilsson@gmail.com>
2025-12-03 13:42:19 +0100

    Test of  "git commit -a"

--- ArduinoBlink/src/main.cpp
+++ ArduinoBlink/src/main.cpp
@@ -1,20 +1,21 @@
 #include <Arduino.h>
-
+#include <LibPrintf.h>
 // put function declarations here:
 
 void setup() {
+  Serial.begin(9600);
   // put your setup code here, to run once:
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-
     // turn the LED on (HIGH is the voltage level)
   digitalWrite(LED_BUILTIN, HIGH);
   // wait for a second
   delay(200);
   // turn the LED off by making the voltage LOW
   digitalWrite(LED_BUILTIN, LOW);
+  // Serial.println( F("Low!") );
    // wait for a second
   delay(200);
 }
